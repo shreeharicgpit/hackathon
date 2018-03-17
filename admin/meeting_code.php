1b79143374732bdd86b7106dd49175bfa8043b78
shreeharicgpit <shreeeharicgpit@gmail.com>
2018-03-17 21:35:39 +0530

    ertyuikl

--- admin/meeting_code.php
+++ admin/meeting_code.php
@@ -2,28 +2,31 @@
 include('header.php');
 include('connect.php');
 ?>
+<?php
+  if(isset($_POST('meeting_code')))
+  {
+    $meeting_code=$_POST['meeting_code'];
+    $sql=mysqli_query($conn,"SELECT * FROM meeting WHERE '$meeting_code' IN (SELECT meeting_code FROM meeting)");
+    if($sql)
+    {
+
+
+    }
+    else {
+      echo "error in fetch";
+    }
+  }
+
+
+?>
         <div class="right_col" role="main">
           <div class="">
             <div class="row">
               <div class="col-md-12">
                     <div class="x_panel" style="">
                         <div class="x_title">
-                            <h2>Schedule Meeting</h2>
-                            <ul class="nav navbar-right panel_toolbox">
-                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Settings 1</a>
-                                        </li>
-                                        <li><a href="#">Settings 2</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                                </li>
-                            </ul>
+                            <h2>Enter Meeting code</h2>
+
                             <div class="clearfix"></div>
                         </div>
                         <div class="x_content">
@@ -31,32 +34,22 @@ include('connect.php');
                             <div class="container">
                                 <div class="row">
 
-                                    <form method="post" action="meeting_details.php">
+                                    <form method="post">
 
                                     <div class='col-sm-4'>
-                                        Meeting Date
-                                        <div class="form-group">
-                                            <div class='input-group date' >
-                                                <input type='date' class="form-control" name="meeting_date" />
+                                      <div class="form-group">
 
-                                            </div>
-                                        </div>
+                                        <input type="text" class="form-control" name="meeting_code">
+
                                     </div>
+                                  </div>
 
-                                     <div class='col-sm-4'>
-                                        Meeting Start Time
-                                        <div class="form-group">
-                                            <div class='input-group date' >
-                                                <input type='time' class="form-control" name="meeting_start_time" />
 
-                                            </div>
-                                        </div>
-                                    </div>
+                                                  <br>
+                                                <div class="form-group">
 
-                                    <div class="form-group">
-                                                <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                                                  <input type="submit" value="Schedule" name="next" class="btn btn-success" value="Next">
-                                                </div>
+                                                  <input type="submit" value="Search" name="submit" class="btn btn-success">
+
                                               </div>
                                     </form>
                                 </div>
@@ -68,5 +61,3 @@ include('connect.php');
             </div>
           </div>
         </div>
-
-        <?php include('footer.php');?>
