43c1f7199983716b02c113ae9093cfe5c0e3a354
shreeharicgpit <shreeeharicgpit@gmail.com>
2018-03-17 21:44:20 +0530

    dfghjk

--- admin/meeting_code.php
+++ admin/meeting_code.php
@@ -1,16 +1,18 @@
 <?php
 include('header.php');
 include('connect.php');
+$_SESSION['meeting_code'];
 ?>
 <?php
   if(isset($_POST('meeting_code')))
   {
     $meeting_code=$_POST['meeting_code'];
-    $sql=mysqli_query($conn,"SELECT * FROM meeting WHERE '$meeting_code' IN (SELECT meeting_code FROM meeting)");
-    if($sql)
+    $sql=mysqli_query($conn,"SELECT * from meeting where meeting_code='$meeting_code'");
+    if(mysqli_num_rows($sql)>0)
     {
-
-
+      $result = mysqli_fetch_assoc($sql);
+      $_SESSION['meeting_code']=$result['meeting_code'];
+      header(location="meeting_update.php");
     }
     else {
       echo "error in fetch";
