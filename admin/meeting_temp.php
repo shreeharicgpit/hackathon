dea144a45ec5fc06906eb2f89d6647e54d8335ff
shreeharicgpit <shreeeharicgpit@gmail.com>
2018-03-18 16:36:44 +0530

    d

--- admin/meeting_temp.php
+++ admin/meeting_temp.php
@@ -5,7 +5,7 @@
 	$result_user="";
 	$row_user="";
 	$meeting_res="";
-	
+
 	$sql1="";
 	$result1="";
 	$row_meeting=array();
@@ -14,17 +14,12 @@
 	{
 		$m_id=$_POST['meeting_id'];
 		$result_user=mysqli_query($conn,"select user_id from attendance where meeting_code='$m_id'");
-		
+
 		$meeting_res=mysqli_query($conn,"select * from meeting where meeting_code='$m_id'");
 		$row_meeting=mysqli_fetch_assoc($meeting_res);
-		
+
 	}
 ?>
-
-
-
-
-
 <div class="right_col" role="main">
     <div class="">
 
@@ -34,8 +29,8 @@
         <div class="row">
           	<div class="col-md-12 col-sm-12 col-xs-12">
 	            <div class="x_panel">
-	              
-	              	
+
+
 	              			<form method="POST" class="form-group row" action="meeting_temp.php">
 							  <div class="col-xs-3">
 												<h3>Enter Meeting Id:</h3>
@@ -48,7 +43,7 @@
 						</form>
 						<br>
 						<br>
-						
+
 						<?php
 							if(isset($_POST['meeting_id']))
 								{
@@ -60,29 +55,28 @@
 						}
 						?>
 
-						
+
 						<table class="table table-responsive table-striped table-bordered table-hover">
 						<?php
 							if(isset($_POST['meeting_id'])){
 								echo "<tr>";
-								echo "<th>User Id</th>";
+
 								echo "<th>Name</th>";
+									echo "<th>Department</th>";
 								echo "<th>Designation</th>";
 								echo "<th>Email</th>";
 								echo "<th>Mobile No.</th>";
-								echo "<th>State</th>";
-								echo "<th>Department</th>";
+
+
 								echo "<th>Attendance</th>";
-								echo "<th>Password</th>";
-								echo "<th>Gender</th>";
-								echo "<th>Address</th>";
-								echo "<th>Profile Pic</th>";
+									echo "<th>State</th>";
+
 								echo "</tr>";
 								while($row_user=mysqli_fetch_assoc($result_user)){
 									$user_id=$row_user['user_id'];
-									$sql1=mysqli_query($conn,"select * from directory where user_id='$user_id'");
+									$sql1=mysqli_query($conn,"select name,department,designation,email,mobile,attendance,state from directory where user_id='$user_id'");
 									$result1=mysqli_fetch_assoc($sql1);
-									
+
 									$status=mysqli_query($conn,"select status from attendance where user_id='$user_id' and meeting_code='$m_id'");
 									$res_status=mysqli_fetch_assoc($status);
 									if($res_status['status']==0)
@@ -94,45 +88,30 @@
 										if($key=="attendance"){
 											if($res_status['status']==0)
 												echo "Absent";
-											else 
+											else
 												echo "Present";
 										}
-										else{											
+										else{
 											echo "$value";
 										}
 										echo "</td>";
 									}
 									echo "</tr>";
 								  }
-									
-							} 
+
+							}
 						?>
 						</table>
 
 	              			</div>
 	              		</div>
-	              	
-	              
+
+
 	      		</div>
         	</div>
     	</div>
     </div>
 </div>
-<?php 
+<?php
 include('footer.php');
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
